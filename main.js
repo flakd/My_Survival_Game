00892bc49a11ff540336e2137b7060fb0dc75e52
Flak DiNenno <flak@dinenno.com>
2022-11-21 09:29:25 -0500

    working with new structure, except allowing fire even though not cold

--- main.js
+++ main.js
@@ -12,16 +12,15 @@ const rl = readline.createInterface({
 const l=console.log;
 const e=console.error
 
+
 var vis = {
   "never": 0,
   "always": 1,
   "GTzero": 2
 }
-
 var time = 0;   //12 midnight
 var timeInterval = 1;  //1 hour
 
-parseJSONs.print();
 parseJSONs.loadJSONs();
 var inventory = parseJSONs.Objects.inventory;
 var actions = parseJSONs.Objects.actions; 
@@ -30,97 +29,34 @@ var i = inventory;
 var a = actions;
 var v = vitals;
 
-l();
-l("Welcome to the game!");
-l();
-printStats1();
+prePlay();
+
 rl.on('line', (line) => {
-  if (line=="c") {
-    //break;
-    console.log("action canceled");
-    printStats1();
-    return;
-  }  
-  if (!actions) {
-    e("**ERROR**:  actions is missing"); 
-    return;
-  }
-  if (actions[line]){
-    if (actions[line].length==0) {
-      e("**ERROR**:  actions[line] is present, but empty/undefined"); 
-      return;
-    }
-  }
+
+  if (!actions) {e("**ERROR**:  actions is missing"); return;}
+
   var action = actions[line];     // e.g.  line = "light"
-  if (action) {   //e.g. action = actions["light"] or actions.light = the "light" line's entire object
+  if (!action) {
+    e("**ERROR**:  action/actions[line] is missing"); return;
+    if (action.length==0) { 
+      e("**ERROR**:  action/actions[line] is present, but empty/undefined"); return;
+    }
+  } else {
 
     var calcSet, 
         gameItemToChange_shortStr,
         gameItemToChange_fullStr,
         gameItemToChange,
-//        gameItemToChange_bal,
-//        gameItemToChange_vis,
         Operator_Str,
         changeAmt
     ;
-
-    //if (line in ["light2", "drink2"]){
-    if (line == "light2"){      
-      check(action, calcSet, gameItemToChange_shortStr, gameItemToChange_fullStr, gameItemToChange, Operator_Str, changeAmt);
-      // attribs is what stats/attributes we're going to change:  AON either inventory or vitals
-      //({ calcSet, gameItemToChange_shortStr, gameItemToChange_fullStr, gameItemToChange, Operator_Str, changeAmt } = newFunction(action, calcSet, gameItemToChange_shortStr, gameItemToChange_fullStr, gameItemToChange, Operator_Str, changeAmt));
-      printStats1();
-    } else {
-      var fromInvGameItem = action.fromInv[0];
-      var fromInvGameItemFull = "i." + fromInvGameItem + "[0]";    
-      var fromInvOperator = action.fromInv[1];
-      var fromInvAmount = action.fromInv[2];
-
-      var toInvGameItem = action.toInv[0];
-      var toInvGameItemFull = "i." + toInvGameItem + "[0]";
-      var toInvOperator = action.toInv[1];
-      var toInvAmount = action.toInv[2];  
-
-      var vitalsGameItem = action.vitals[0];
-      var vitalsGameItemFull = "vitals." + vitalsGameItem + "[0]";
-      var vitalsOperator = action.vitals[1];
-      var vitalsAmount = action.vitals[2];  
-      {
-        var fromInvEvalStrCond = fromInvGameItemFull + fromInvOperator + fromInvAmount;
-        var vitalsEvalStrCond = vitalsGameItemFull + vitalsOperator + vitalsAmount;
-        if (eval(fromInvEvalStrCond) < 0) {
-          console.log("Sorry, you need to have at least %i %s to do that - but, you (only) have %i %s!", fromInvAmount, fromInvGameItem, i[fromInvGameItem], fromInvGameItem )
-          return;
-        } 
-        if (eval(vitalsEvalStrCond) < 0)  {
-          console.log("You don't feel like doing that -- you have no %s", vitalsGameItem )
-          return;
-        }       
-        var fromInvEvalStrDo  = fromInvGameItemFull 
-                              + ( (fromInvOperator != "=") 
-                                ? ("=" + fromInvGameItemFull + fromInvOperator + fromInvAmount) 
-                                : (fromInvOperator + fromInvAmount)
-        );   
-        var toInvEvalStrDo    = toInvGameItemFull 
-                              + ( (toInvOperator != "=") 
-                                ? ("=" + toInvGameItemFull + toInvOperator + toInvAmount) 
-                                : (toInvOperator + toInvAmount)
-        );      
-        var vitalsEvalStrDo    = vitalsGameItemFull 
-                              + ( (vitalsOperator != "=") 
-                                ? ("=" + vitalsGameItemFull + vitalsOperator + vitalsAmount) 
-                                : (vitalsOperator + vitalsAmount)
-        );                
-        eval(fromInvEvalStrDo);
-        eval(toInvEvalStrDo);
-        eval(vitalsEvalStrDo);
-        printStats1(action.verb);
-        time+=timeInterval;
-      }
-    } //elseIF (line != "light2")
+    
+    check(action, calcSet, gameItemToChange_shortStr, gameItemToChange_fullStr, gameItemToChange, Operator_Str, changeAmt);
+    // attribs is what stats/attributes we're going to change:  AON either inventory or vitals
+    //({ calcSet, gameItemToChange_shortStr, gameItemToChange_fullStr, gameItemToChange, Operator_Str, changeAmt } = newFunction(action, calcSet, gameItemToChange_shortStr, gameItemToChange_fullStr, gameItemToChange, Operator_Str, changeAmt));
+    printStats1();
     return;
   }
-
 
   if (line == "QUIT") {
     console.log("QUIT is true");
@@ -138,9 +74,7 @@ rl.once('close', () => {
   console.log("Thank you for playing!")
     // end of input
   exit();
- });
-
-
+});
 
 function check(action, calcSet, gameItemToChange_shortStr, gameItemToChange_fullStr, gameItemToChange, Operator_Str, changeAmt) {
   for (var attribsLbl in action) { // e.g. attribsLbl = "inventory"
@@ -196,8 +130,6 @@ function check(action, calcSet, gameItemToChange_shortStr, gameItemToChange_full
   //return { calcSet, gameItemToChange_shortStr, gameItemToChange_fullStr, gameItemToChange, Operator_Str, changeAmt };
 }
 
-
-
 function printStats1(verb){
   l("**********  TIME: %s", ( ('0' + (time%24)).slice(-2) + ':00 hrs' )  );
   var msg = "";
@@ -223,17 +155,19 @@ function printStats2(list){
   var msg ="  ";
   var val, amt, visible;
   
+  if (!list) {l("**ERROR**:  list is missing"); return;}
   for (key in list){
     val = list[key];
-    if (key.slice(4) == "2") {
+    if (!val) {l("**ERROR**:  val is missing"); return;}
+    //if (key.slice(4) == "2") {
       //console.log(key);
       amt = val.bal;
       visible = val.vis;
-    } else {
-      amt = val[0];
-      visible = val[1];
-      //if (visible!=null && amt > 0 ){
-    }
+    //} else {
+    //  amt = val[0];
+    //  visible = val[1];
+    //  //if (visible!=null && amt > 0 ){
+    //}
     if (visible == "never") continue;
     if (visible == "GTzero" && amt < 1) continue;    
     if (  (visible == "GTzero" && amt > 0)
@@ -256,4 +190,11 @@ function printStats2(list){
   }
   if (msg !="") l(msg);
   return numListItems;
+}
+
+function prePlay(){
+  l();
+  l("Welcome to the game!");
+  l();
+  printStats1();  
 }
