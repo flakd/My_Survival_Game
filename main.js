e1f261e49b465bea1f64d7b7c1e5e2dba4704de0
Flak DiNenno <flak@dinenno.com>
2022-11-23 05:14:37 -0500

    light is working except not incrementing time

--- main.js
+++ main.js
@@ -1,9 +1,7 @@
-const { checkPrime } = require('crypto');
 const { exit } = require('process');
 const readline = require('readline');
 const parseJSONs = require('./parseJSONs');
-
-
+const second =  require('./second');
 const rl = readline.createInterface({
   input: process.stdin,
   output: process.stdout,
@@ -12,123 +10,63 @@ const rl = readline.createInterface({
 const l=console.log;
 const e=console.error
 
-
-var vis = {
-  "never": 0,
-  "always": 1,
-  "GTzero": 2
-}
 var time = 0;   //12 midnight
 var timeInterval = 1;  //1 hour
+var isQuit = false;
 
 parseJSONs.loadJSONs();
 var inventory = parseJSONs.Objects.inventory;
 var actions = parseJSONs.Objects.actions; 
-var vitals = parseJSONs.Objects.vitals; 
-var i = inventory;
-var a = actions;
-var v = vitals;
-
-prePlay();
+var vitals = parseJSONs.Objects.vitals;
+printTitleBanner();
 
 rl.on('line', (line) => {
-
+  
   if (!actions) {e("**ERROR**:  actions is missing"); return;}
 
   var action = actions[line];     // e.g.  line = "light"
-  if (!action) {
-    e("**ERROR**:  action/actions[line] is missing"); return;
+  if (action) {
     if (action.length==0) { 
-      e("**ERROR**:  action/actions[line] is present, but empty/undefined"); return;
+      e("**ERROR**:  action(actions[line]) is present, but empty or undefined"); return;
     }
-  } else {
-
-    var calcSet, 
-        gameItemToChange_shortStr,
-        gameItemToChange_fullStr,
-        gameItemToChange,
-        Operator_Str,
-        changeAmt
-    ;
-    
-    check(action, calcSet, gameItemToChange_shortStr, gameItemToChange_fullStr, gameItemToChange, Operator_Str, changeAmt);
+    second.check(inventory, vitals, action, time, timeInterval);
     // attribs is what stats/attributes we're going to change:  AON either inventory or vitals
-    //({ calcSet, gameItemToChange_shortStr, gameItemToChange_fullStr, gameItemToChange, Operator_Str, changeAmt } = newFunction(action, calcSet, gameItemToChange_shortStr, gameItemToChange_fullStr, gameItemToChange, Operator_Str, changeAmt));
     printStats1();
     return;
-  }
+  } else {
+    //e("**ERROR**:  action/actions[line] is missing"); return;
+    if (line == "QUIT") {
+      //console.log("QUIT is true");
+      l("Are you sure you want to quit? (y/n, Y/N");
+      isQuit = true;
+    }    
+    if (isQuit) {
+      if (line=="y" || line=="Y") {
+        console.log("OK then.  Thank you for playing!  Goodbye and play again soon!");
+        rl.close();
+      } else 
+      if (line=="n" || line=="N") {
+        isQuit = false;
+        printStats1();
+        return;
+      }
+    } else {
 
-  if (line == "QUIT") {
-    console.log("QUIT is true");
-    rl.close();
-    return; //skip the rest of the conditions
+      //CATCH Default/Else: whatever input("line") is, it's unrecognized
+      l("'%s' line is not recognized. Please try again\n",line);
+      printStats1();
+      return; //loop again into another readline
+    }
   }
-  
-  //CATCH Default/Else: whatever input("line") is, it's unrecognized
-  l("'%s' line is not recognized. Please try again\n",line);
-  printStats1();
-  return; //loop again into another readline
 });
 
 rl.once('close', () => {
-  console.log("Thank you for playing!")
-    // end of input
-  exit();
+  // end of input
+  exit();  
 });
 
-function check(action, calcSet, gameItemToChange_shortStr, gameItemToChange_fullStr, gameItemToChange, Operator_Str, changeAmt) {
-  for (var attribsLbl in action) { // e.g. attribsLbl = "inventory"
-    var attribs = action[attribsLbl];
 
-    for (var attribLbl in attribs) { //e.g. attrib = "calcs" or "msgs"
-      var attrib = attribs[attribLbl];
 
-      if (attribLbl == "calcs") {
-        var calcs = attrib; // calcs=attrib = obj of subs and adds to inventory or vitals
-
-        for (var calcLbl in calcs) { //e.g. calcLbl (string) = "sub" or "add"
-          calcSet = calcs[calcLbl]; // e.g. ENTIRE add or sub PROPERTY
-          gameItemToChange_shortStr = calcSet.gameItem; // e.g. "wood" (string)
-          gameItemToChange_fullStr = attribsLbl + "." + gameItemToChange_shortStr;
-          gameItemToChange = eval(gameItemToChange_fullStr);
-          //var gameItemToChange_bal = gameItemToChange.bal;
-          //var gameItemToChange_vis = gameItemToChange.vis;
-          Operator_Str = calcSet.operator;
-          changeAmt = calcSet.changeAmt;
-
-          var doAction_evalStr = "";
-          //var doAction = function(){};
-          if (Operator_Str == "=") {
-            doAction_evalStr = "gameItemToChange.bal" + Operator_Str + changeAmt;
-            var tmp = 0;
-          }
-          else if (Operator_Str == "+" || Operator_Str == "-") {
-            doAction_evalStr = "gameItemToChange.bal" + "=" + "gameItemToChange.bal" + Operator_Str + changeAmt;
-          }
-          var doActionCond_evalStrs = {
-            "inventory":  "gameItemToChange.bal" + Operator_Str + changeAmt + " >= 0 ",
-            "vitals":     "gameItemToChange.bal" + gameItemToChange.doOper
-          }
-          var errMsg = "";
-
-          if ( !eval(doActionCond_evalStrs[attribsLbl]) ){
-            if (attribsLbl=="inventory"){
-              l(attribs.msgs.errMsg, changeAmt, gameItemToChange_shortStr, gameItemToChange.bal, gameItemToChange_shortStr);                
-            } else 
-            if (attribsLbl=="vitals"){
-              l(attribs.msgs.errMsg);
-            }
-            return;
-          } else {
-            eval(doAction_evalStr);
-          }          
-        }
-      }
-    }
-  }
-  time+=timeInterval;        
-  //return { calcSet, gameItemToChange_shortStr, gameItemToChange_fullStr, gameItemToChange, Operator_Str, changeAmt };
-}
 
 function printStats1(verb){
   l("**********  TIME: %s", ( ('0' + (time%24)).slice(-2) + ':00 hrs' )  );
@@ -192,7 +130,7 @@ function printStats2(list){
   return numListItems;
 }
 
-function prePlay(){
+function printTitleBanner(){
   l();
   l("Welcome to the game!");
   l();
---
+++ second.js
@@ -0,0 +1,105 @@
+const l = console.log;
+
+let second = {
+  check: function check(inventory, vitals, action, time, timeInterval) {
+    var calcSet, 
+        gameItemToChange_shortStr,
+        gameItemToChange_fullStr,
+        gameItemToChange,
+        Operator_Str,
+        changeAmt    
+    ;    
+    var doAction_evalStrs = {};
+    var inventoryDflt_cond_evalStr = {};
+    var gameItemsToChange_evalStrs = {};
+    var numTrue=0;
+    var numCalcs=0;
+
+
+    for (var attribsLbl in action) { // e.g. attribsLbl = "inventory" or "vitals"
+      var attribs = action[attribsLbl];
+
+      for (var attribLbl in attribs) { //e.g. attrib = "calcs" or "msgs"
+        var attrib = attribs[attribLbl];
+
+        if (attribLbl == "calcs") {
+          var calcs = attrib; // calcs=attrib = obj of subs and adds to inventory or vitals
+
+          for (var calcLbl in calcs) { //e.g. calcLbl (string) = "take" or "give"
+            numCalcs++;
+            calcSet = calcs[calcLbl]; // e.g. ENTIRE add or sub PROPERTY
+            gameItemToChange_shortStr = calcSet.gameItem; // e.g. "wood" (string)
+            gameItemToChange_fullStr = attribsLbl + "." + gameItemToChange_shortStr;
+            gameItemToChange = eval(gameItemToChange_fullStr);
+            Operator_Str = calcSet.operator;
+            changeAmt = calcSet.changeAmt;
+
+            var doAction_evalStr = "";
+            if (Operator_Str == "=") {
+              //doAction_evalStr = "gameItemToChange.bal" + Operator_Str + changeAmt;
+              doAction_evalStr = gameItemToChange_fullStr + ".bal" + Operator_Str + changeAmt; 
+              var tmp = 0;
+            }
+            else if (Operator_Str == "+" || Operator_Str == "-") {
+              doAction_evalStr = gameItemToChange_fullStr + ".bal" + "=" + gameItemToChange_fullStr + ".bal" + Operator_Str + changeAmt;
+            }
+
+            var tmpCondEval =  "gameItemToChange.bal" + Operator_Str + changeAmt;
+            eval(tmpCondEval);
+            inventoryDflt_cond_evalStr[attribsLbl+"_"+calcLbl] = tmpCondEval + ">= 0"
+            var vitalsTake_cond_evalStr;
+            var vitalsGive_cond_evalStr;
+
+            if ( !eval(inventoryDflt_cond_evalStr[attribsLbl+"_"+calcLbl]) ){
+              if (attribsLbl=="inventory"){
+                l(attribs.msgs.errMsg, changeAmt, gameItemToChange_shortStr, gameItemToChange.bal, gameItemToChange_shortStr);                
+                return;
+              }          
+            } else {
+              doAction_evalStrs[attribsLbl+"_"+calcLbl] = doAction_evalStr;
+            }          
+            if (attribsLbl=="vitals"){
+              if (calcSet=="take"){
+                vitalsTake_cond_evalStr = "gameItemToChange.bal" + Operator_Str + changeAmt + "gameItemToChange.dieOper" + "gameItemToChange.dieLimit";
+                if (eval(vitalsTake_cond_evalStr)) {
+                  l( eval("gameItemToChange.dieMsg") );   //e.g. you died... "of exhaustion", "by freezing to death", etc.
+                  return;   // TODO:  change to EXIT (game over)
+                } else {
+                  doAction_evalStrs[attribsLbl+"_"+calcLbl] = doAction_evalStr;
+                }
+              }
+              if (calcSet=="give"){
+                vitalsGive_cond_evalStr = "gameItemToChange.bal" + Operator_Str + changeAmt + "gameItemToChange.doOper" + "gameItemToChange.doLimit";
+                if (eval(vitalsGive_cond_evalStr)) {
+                  l( eval("gameItemToChange.doErr") );    //e.g. "you're not cold", "you're not hungry"
+                  return;   // TODO:  change to EXIT (game over)
+                } else {
+                  doAction_evalStrs[attribsLbl+"_"+calcLbl] = doAction_evalStr;
+                }
+              } 
+            }
+ 
+            
+            /*none of those caused a return do to failure, so add 
+              THE ONE doAction_evalStr (there is onely one generic 
+              for all 4 possibilities) to the object
+            */
+            //doAction_evalStrs[attribsLbl+"_"+calcLbl] = doAction_evalStr;
+            numTrue++;
+          
+          }
+        }
+      }
+    }
+    if (numTrue == numCalcs){
+      for (var doAction_evalStr2 in doAction_evalStrs){
+        eval(doAction_evalStrs[doAction_evalStr2]);
+        time+=timeInterval;        
+      }
+      //eval(doAction_evalStr);
+    }
+    //return { calcSet, gameItemToChange_shortStr, gameItemToChange_fullStr, gameItemToChange, Operator_Str, changeAmt };
+  }
+}
+
+module.exports = second;
