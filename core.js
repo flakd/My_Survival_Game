65bdd491654486f9991f5f8cc7bf26ae79b37de9
Flak DiNenno <flak@dinenno.com>
2022-12-01 12:20:46 -0500

    separating canDoInvTakeCalcs from doInvTakeCalcs

--- core.js
+++ core.js
@@ -11,7 +11,7 @@ const l=console.log;
 const e=function(msg){console.error("**ERROR**: %s",msg)};
 
 let core = {
-  gameMainLoop: function gameMainLoop(userInput, inventoryOrig, vitalsOrig, actions){
+  isStillAliveGameLoop: function isStillAliveGameLoop(userInput, inventoryOrig, vitalsOrig, actions){
     //  ** ANY FAILURE below immediately jumps out of function **
     
     //  0. print "status" 
@@ -76,7 +76,10 @@ let core = {
     if ( doRandomActOfGod(inventory, vitals) ) {
       output.printStats1(g.gameHour,g.c);
     }
-    return (!isDead(vitals));
+    return (!isDead(vitals)); // if you're dead, this is FALSE, which triggers 
+                              //  an rl.close() of the main  game READLINE 
+                              //  loop - but we want to confirm that you are 
+                              //  dead, or that you've quit
 
     //  5. pass time (update any time-dependent variables )
     //     a. based on action.duration * vitals.COST
@@ -156,7 +159,12 @@ let core = {
         }
 
       }
-      return (numDeaths > 0);
+      if (numDeaths > 0) {
+        g.isDead = true;
+        return true;  // we are returning TRUE for this function - isDead()
+                      //  - which will trigger a return of false to the main 
+                      //  loop (see above:  "return (!isDead(vitals));"
+      }
     }
 
     // print status at the end... AFTER the command is executed, 
@@ -167,12 +175,10 @@ let core = {
     //  8. loop back to beginning
     return true;
 
-
   },
 
-  printStatus: function printStatus(){
-    l("printing status");
-  },
+
+
 
   doSecretTestCalc: function doSecretTestCalc(userInput, inventory, vitals){
     if ( userInput.toLowerCase() == "/p" ) {
@@ -211,7 +217,6 @@ let core = {
       return false;
     }
   },
-
 
   isInputAValidGameCommand: function isInputAValidGameCommand(userInput, inventory, vitals){
     switch (userInput.toLowerCase()){
@@ -345,7 +350,7 @@ let core = {
         }
       }
       return invTakeCalcs;
-    }
+    } // END: getInvTakeCalcs(calcs)
 
     function canDoInvTakeCalc(calc) {
       // store the full string/name which we need to use to build an 
@@ -403,7 +408,7 @@ let core = {
         l(inventory.none.dflt_doFailMsg, calc.changeAmt, calc.item, calc.preCalcBal, calc.item);
         return false;   // return false b/c this calc failed        
       }
-    } // END:  doInvTakeCalc
+    } // END:  canDoInvTakeCalc
   }, // END:  function canPerformAction(action, inventory, vitals){
 
 
@@ -414,6 +419,118 @@ let core = {
   doGameAction: function doGameAction(action, inventory, vitals){
     doInvGiveCalcs(action,inventory);
     doVitGiveCalcs(action, vitals);
+
+
+
+    function doInvTakeCalcs(action) {
+      //prepare for keeping tracking of ANY of these InvTakeCalc conditions fail
+      var numInValidTakeConds = 0;   // a successful command means this > 0 when we're done
+
+      // we're only looking at the CALCS prop of the ACTION object, we don't
+      //  care about the other properties !="calcs" (e.g. key, duration, msgs)
+      var invTakeCalcs = getInvTakeCalcs(action.calcs);
+
+      // if we don't find any invTakeCalcs (null/undefined) then print a developer error??
+      if (!(invTakeCalcs)) { e("missing invTakeCalcs"); }
+            
+      for (var calcIdx in invTakeCalcs){
+        if (!doInvTakeCalc(invTakeCalcs[calcIdx]))         
+        // TRUE: numInValidConds++  (now it will be greater than 0 if 
+        //  false (meaning at least one failed)
+        numInValidTakeConds++;
+
+        //  => this means we don't perform the actiion [all have to be 
+        //  successful, i.e. you need to have enough of EACH AND EVERY/ALL
+        //   if the inventory and vitals to perform this calculation/action])    
+      }          
+      // if ANY of these tests fail then RETURN FALSE ==>
+      //  this means we CANNOT perform the action
+      return (numInValidTakeConds == 0)
+
+
+      function getInvTakeCalcs(calcs) {
+        // prepare to store all the "inventory takes" in an array
+        var invTakeCalcs = [];
+
+        for (var calcIdx in action.calcs) { // array iteration instead of object iteration
+          // let's look through each item in the "calcs" property/subObject
+          // lets make it easier to refer to the actual indicidual "calc item"
+          //  that we're looking at, at the moment
+          var calc = action.calcs[calcIdx];
+
+          // the name of the GameItem we want to CALC FROM as a string is 
+          //  stored in calc.list and calc.item combined
+          if (!calc.list || !calc.item) { e("missing ITEM NAME (list & item)"); }
+
+          if (calc.list=="inventory" && calc.type=="take") {
+            invTakeCalcs.push(calc);
+          }
+        }
+        return invTakeCalcs;
+      }
+
+      function doInvTakeCalc(calc) {
+        // store the full string/name which we need to use to build an 
+        //  actual code reference to the gameItem object
+        var gameItem_fullEvalStr = calc.list + "." + calc.item;
+        var gameItemBal_evalStr = gameItem_fullEvalStr + ".bal";
+
+        // now store a reference to the ACTUAL GameItem by 'eval'ing the 
+        //  STRING NAME of the GameItem, which is the string in calc.itemStr/calcItem_fullLbl
+        calc.gameItem = eval(gameItem_fullEvalStr);
+        if (!calc.gameItem) {e("possible misspelling of gameItem in JSON");}
+
+        // store original balance to use in error message, in case this calculation fails
+        calc.preCalcBal = calc.gameItem.bal;
+                          
+        // if the preCalcBal MINUS the amount to change (subtract, since 
+        //  this is a TAKE operation) equals less than ZERO, then we didn't
+        //  have enough of this inventory item/resource to perform this
+        //  in the first place => NOW => store True/False in boolean var
+        calc.willCalcCondFail_str = "(preCalcBal - calc.changeAmt < 0)";
+        calc.willCalcCondFail_str2 = "(" + calc.preCalcBal + "-" + calc.changeAmt + " < 0" + ")";
+        calc.willCalcCondFail = (calc.preCalcBal - calc.changeAmt < 0);
+
+        // set up the ACTUAL 'perform calculation' statement      
+        var doTakeCalc_evalStr = gameItemBal_evalStr + "=" + gameItemBal_evalStr + calc.operator + calc.changeAmt;
+        // e.g. (doTakeCalc_evalStr = "inventory.wood = inventory.wood - 5"  or  "vitals.hunger = vitals.hunger + 10"
+
+        // go RIGHT AHEAD and perform the operation on THIS CLONED copy by EVALing the "doTakeCalc string"
+        eval (doTakeCalc_evalStr);
+
+        // store post calculation balance for comparison (?)                    
+        calc.postCalcBal = calc.gameItem.bal;
+        // not sure we use this ????
+
+        //print these regardless of whether it's false(succeeded) or true(failed)
+        l("postCalcBal:"+calc.postCalcBal);            
+        l();
+        if (!calc.willCalcCondFail){ // it's FALSE, that means the calc SUCCEEDED, 
+          l("This calc (%s) SUCCEEDED: %s[%s] is %s", 
+              doTakeCalc_evalStr, calc.willCalcCondFail_str, 
+              calc.willCalcCondFail_str2, calc.willCalcCondFail
+          );                  
+          return true;  // return true if that calc succeeds
+        } else  { // it's TRUE, that means the calc FAILED
+                  //  so print error msg and increase 
+                  //  failure number by one (if we have 
+                  //  even 1 failure, then we won't allow 
+                  //  this ACTION [i.e. we won't copy 
+                  //  this clone back to the original])
+                  
+          l("This calc (%s) FAILED: %s[%s] is %s", 
+              doTakeCalc_evalStr, calc.willCalcCondFail_str, 
+              calc.willCalcCondFail_str2, calc.willCalcCondFail
+          );      
+          l(inventory.none.dflt_doFailMsg, calc.changeAmt, calc.item, calc.preCalcBal, calc.item);
+          return false;   // return false b/c this calc failed        
+        }
+      } // END:  doInvTakeCalc
+    } // END:  doInvTakeCalcs
+
+
+
+
     function doInvGiveCalcs(action, inventory){
      // we're only looking at the CALCS prop of the ACTION object, we don't
       //  care about the other properties !="calcs" (e.g. key, duration, msgs)
