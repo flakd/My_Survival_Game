2a2ed14d98557d636657e108075bf2bdc47f663d
Flak DiNenno <flak@dinenno.com>
2022-12-26 12:43:17 -0500

    synced sun and moon with 24 hour clock (but this is wrong, needs 16hr & 8hr, not 24 + 24

--- js/sunOrMoonRising.js
+++ js/sunOrMoonRising.js
@@ -13,9 +13,11 @@ function moveSunOrMoon(sunMoon){
     if (sunMoon === "sun") {
       moon.style.display = "none";
       sun.style.display = "block";
+      document.querySelector("#sky-box").style.backgroundColor = "skyblue";
       sunOrMoon = sun;
     } else if (sunMoon === "moon"){
       moon.style.display = "block";
+      document.querySelector("#sky-box").style.backgroundColor = "#7777FF";
       sun.style.display = "none";
       sunOrMoon = moon;
     }
@@ -25,6 +27,14 @@ function moveSunOrMoon(sunMoon){
   let timer = setInterval(function() {
     let timePassed = Date.now() - start;
     lwr(timePassed,document.querySelector("#log2"));
+
+    g.gameHour = Math.floor(timePassed / 2650);
+    //console.log(g.gameHour);
+    //document.querySelector("#time").innerHTML = g.gameHour + "";
+    var timeStr = g.gameHour.toString().padStart(2,"0");
+    lwrTime(`${timeStr}Hrs`);
+    document.getElementById("command-input").focus();
+
 
     // sun/moon RISING PHASE
     if (timePassed < 9000) {
@@ -38,14 +48,14 @@ function moveSunOrMoon(sunMoon){
     } 
     
     // sun/moon SETTING PHASE
-    else if (timePassed >= 55000 && timePassed < 64000) {
+    else if (timePassed >= 55000 && timePassed < 63600) {
       lwln(sunOrMoon.style.top);
       sunOrMoon.style.left = ((timePassed / 150) -70) + 'px';
       sunOrMoon.style.top = ( (timePassed / 75) - 744)  + 'px';
     }
 
     // sun or moon (day or night) lasts 64 seconds, then we switch to the other
-    if (timePassed >= 64000) {
+    if (timePassed >= 63600) {
       clearInterval(timer);
       if (sunMoon=="sun") {
         sunMoon="moon";
