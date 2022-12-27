4009dc5a43006fe400f5e5edc4e434031ea25353
Flak DiNenno <flak@dinenno.com>
2022-12-26 19:04:51 -0500

    no recursive call for timer, 16hr/8hr split for day/night

--- js/sunOrMoonRising.js
+++ js/sunOrMoonRising.js
@@ -22,41 +22,58 @@ function moveSunOrMoon(sunMoon){
       sunOrMoon = moon;
     }
   }
-  let start = Date.now();
+
+  g.time.start = Date.now();
+  g.time.gameDay = 1;
+  var dayStr = g.time.gameDay.toString().padStart(3,"0");
+  lwrDay(`DAY:${dayStr}`);  
 
   let timer = setInterval(function() {
-    let timePassed = Date.now() - start;
-    lwr(timePassed,document.querySelector("#log2"));
+    g.time.passed = Date.now() - g.time.start;
+    lwr(g.time.passed,document.querySelector("#log2"));
 
-    g.gameHour = Math.floor(timePassed / 2650);
-    //console.log(g.gameHour);
-    //document.querySelector("#time").innerHTML = g.gameHour + "";
-    var timeStr = g.gameHour.toString().padStart(2,"0");
-    lwrTime(`${timeStr}Hrs`);
+    g.time.oneHour = 5300;
+    g.time.gameHour = Math.floor(g.time.passed / g.time.oneHour);
+    g.time.dayStartHour = 0;
+    g.time.dayEndHour = 24;
+    g.time.sunriseHour = 5; // 5am
+    g.time.sunsetHour = 20; // 9pm
+    g.time.riseSetDuration = 4500;    
+
+    var hourStr = g.time.gameHour.toString().padStart(2,"0");
+    lwrTime(`${hourStr}Hrs`);
+
     document.getElementById("command-input").focus();
 
 
     // sun/moon RISING PHASE
-    if (timePassed < 9000) {
-      sunOrMoon.style.left = ((timePassed / 150) -70) + 'px';
-      sunOrMoon.style.top = (110 + (timePassed / 75 * -1))  + 'px';
+    if (g.time.passed < g.time.riseSetDuration) {
+    //if (g.gameHour < 0) {}
+      sunOrMoon.style.left = ((g.time.passed / 200) -70) + 'px';
+      sunOrMoon.style.top = (110 + (g.time.passed / 40 * -1))  + 'px';
     } 
     
-    else if (timePassed >= 9000 && timePassed < 55000) {
-      sunOrMoon.style.left = ((timePassed / 150) -70) + 'px';
-      //sunOrMoon.style.top = ( (timePassed / 75) - 90)  + 'px';
+    //else if (timePassed >= 9000 && timePassed < 55000) {
+    else if (g.time.passed >= g.time.riseSetDuration && (g.time.passed < g.time.sunsetHour * 5300) ) {
+      sunOrMoon.style.left = ((g.time.passed / 200) -70) + 'px';
     } 
     
     // sun/moon SETTING PHASE
-    else if (timePassed >= 55000 && timePassed < 63600) {
+    else if (g.time.passed >= 55000 && g.time.passed < 82800) {
       lwln(sunOrMoon.style.top);
-      sunOrMoon.style.left = ((timePassed / 150) -70) + 'px';
-      sunOrMoon.style.top = ( (timePassed / 75) - 744)  + 'px';
+      sunOrMoon.style.left = ((g.time.passed / 200) -70) + 'px';
+      sunOrMoon.style.top = ( (g.time.passed / 75) - 744)  + 'px';
     }
 
     // sun or moon (day or night) lasts 64 seconds, then we switch to the other
-    if (timePassed >= 63600) {
-      clearInterval(timer);
+    if (g.time.passed >= 82800) {
+      //clearInterval(timer);
+      g.time.start = Date.now();
+      g.time.gameDay++;
+
+      dayStr = g.time.gameDay.toString().padStart(3,"0");
+      lwrDay(`DAY:${dayStr}`);
+
       if (sunMoon=="sun") {
         sunMoon="moon";
       } else if (sunMoon=="moon") {
